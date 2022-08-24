136cd94fd1054fa211902ea134472c03b92e4f24
LuckyDice1 <minerhymes@gmail.com>
2022-08-24 10:11:13 -0500

    2nd Day Commits

---
+++ Java 111/src/Welcome to Java Starter Programs/WelcomeToJavaThreeMessages.java
@@ -0,0 +1,9 @@
+import javax.sound.midi.Soundbank;
+
+public class WelcomeToJavaThreeMessages {
+    public static void main(String[] args) {
+        System.out.println("Some String 1");
+        System.out.println("Some string 2");
+        System.out.println("Some string 3");
+    }
+}
