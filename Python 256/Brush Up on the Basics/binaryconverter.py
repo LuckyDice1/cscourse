05b446ab0ffff3e33ae1f0f34a92725f308f6400
LuckyDice1 <minerhymes@gmail.com>
2022-08-29 16:45:07 -0500

    Directory Movement

--- Python 256/Brush Up on the Basics/binaryconverter.py
+++ Python 256/Brush Up on the Basics/binaryconverter.py
@@ -1,23 +1,34 @@
 import numpy as np
 
-decision = int(input("1. Binary -> Decimal\n2. Decimal -> Binary:\n>> ")) 
-if decision == 1:
-    num = input("Your binary number: ")
+def integer_to_binary(num):
+        starting_exponent = round(np.log2(int(num)))
+        result = ""
+        for i in range(starting_exponent, -1, -1):
+            if num >= 2**i:
+                result += '1'
+                num -= 2**i
+            elif num < 2**i:
+                result += '0'
+        return int(result)
+
+def binary_to_integer(num):
     result = 0
     for idx, i in enumerate(reversed(num)):
         result += (2**idx)*int(i)
-    print(result)
+    return(result)
+
+
+decision = int(input("1. Binary -> Decimal\n2. Decimal -> Binary:\n3. Binary -> Hexidecimal\n>> ")) 
+if decision == 1:
+    num = input("Your binary number: ")
+    print(binary_to_integer(num))
 elif decision == 2:
     num = int(input("Input your integer: "))
-    starting_exponent = round(np.log2(int(num)))
-    result = ""
-    for i in range(starting_exponent, -1, -1):
-        if num >= 2**i:
-            result += '1'
-            num -= 2**i
-        elif num < 2**i:
-            result += '0'
-    print(int(result))
+    print(integer_to_binary(num))
+elif decision == 3:
+    ...
+
+    
         
 
 
